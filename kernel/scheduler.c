fe6b365a3ab5e4706630f8e01b86d3bedf55584f
Aditya Rajput <adityandr8274@gmail.com>
2026-08-18 21:55:34 +0530

    Added Comments in 'scheduler.c'

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -7,6 +7,7 @@
 
 uint32_t readyBitMap = 0;
 
+/* Initializes the RTOS Scheduler */
 void schedulerInit(tcb_t* tcbQueue){
 	for (uint32_t i=0; i<NO_OF_THREADS; i++){
 		readyQueue[i].head = NULL;
@@ -15,6 +16,7 @@ void schedulerInit(tcb_t* tcbQueue){
     readyBitMap = 0;
 }
 
+/* Finds the Highest Priority out of the Ready Queue */
 uint32_t findHighestPriority(void){
     if (readyBitMap == 0){
         return 0;
@@ -22,6 +24,7 @@ uint32_t findHighestPriority(void){
     return 31 - __builtin_clz(readyBitMap);
 }
 
+/* Finds the next highest priority task to allowed to run */
 void selectNextTask(void){
 	tcb_t *task;
 	uint32_t priority;
@@ -54,15 +57,9 @@ void selectNextTask(void){
     pCurrentTcb = task;
 }
 
+/* Yields the task */
 void taskYield(void){
     uint32_t mask = enterCritical();
     portNVIC_INT_CTRL = portNVIC_PENDSVSET_BIT;
     exitCritical(mask);
 }
-
-
-
-
-
-
-
